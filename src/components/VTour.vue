f09cb2bf447593655a70fce76f3127d49834fcac
JungMinHur <jungmin@thegarage.kr>
2019-06-13 10:16:08 +0900

    watch removed

--- src/components/VTour.vue
+++ src/components/VTour.vue
@@ -59,8 +59,7 @@ export default {
     }
   },
   mounted () {
-    // this.$tours[this.name] = this
-
+    this.$tours[this.name] = this
     if (this.customOptions.useKeyboardNavigation) {
       window.addEventListener('keyup', this.handleKeyup)
     }
@@ -71,12 +70,12 @@ export default {
       window.removeEventListener('keyup', this.handleKeyup)
     }
   },
-  watch: {
-    name(newValue) {
-      console.log('this is newValue', newValue)
-      this.$tour[newValue] = this
-    }
-  },
+  // watch: {
+  //   name(newValue) {
+  //     console.log('this is newValue', newValue)
+  //     this.$tour[newValue] = this
+  //   }
+  // },
   computed: {
     // Allow us to define custom options and merge them with the default options.
     // Since options is a computed property, it is reactive and can be updated during runtime.
